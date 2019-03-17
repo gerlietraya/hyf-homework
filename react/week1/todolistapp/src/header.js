a8a4a91ed2d1b01158c375c40576aa8e4ce482af
Gerlie Traya <navasquezgerlie@yahoo.com>
2019-03-18 00:25:48 +0100

    added header.js.index.css and index.js and images file

---
+++ react/week1/todolistapp/src/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1 id="title">TODO LIST</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ react/week1/todolistapp/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import Header from "./header";
+
+ReactDom.render(<Header />,document.getElementById("root"));
